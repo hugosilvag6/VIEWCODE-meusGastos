41b72c4fcceebd28e97959c3f772c3e14aa52d54
hugosilvag6 <hugosilvag6@hotmail.com>
2022-11-23 22:55:20 -0300

    Initial Commit

---
+++ MeusGastos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MeusGastos
+//
+//  Created by Hugo Silva on 23/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
